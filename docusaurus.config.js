e4540c8dfc455e518200982fc6d53bc78c141b3a
oriooctopus <oliverullman@gmail.com>
2020-06-08 16:55:16 -0600

    LIB-586: Migrate to shared repo of components part 1

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,84 +6,27 @@ module.exports = {
   tagline: 'The Libra Association’s mission is to enable a simple global payment system and financial infrastructure that empowers billions of people.',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
-  favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  favicon: 'img/shared/favicon.ico',
+  organizationName: 'Libra',
+  projectName: 'Libra',
   themeConfig: {
     algolia: {
       apiKey: 'eb29b473d27eae9cc46c84eb3a2e4063',
       indexName: 'libra-website',
     },
-    sidebarCollapsible: false,
-    navbar: {
-      title: 'My Site',
-      logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo.svg',
-      },
-      links: [
-        {
-          to: 'docs/doc1',
-          activeBasePath: 'docs',
-          label: 'Docs',
-          position: 'left',
-        },
-        {to: 'blog', label: 'Blog', position: 'left'},
-        {
-          href: 'https://github.com/facebook/docusaurus',
-          label: 'GitHub',
-          position: 'right',
-        },
-      ],
+    prism: {
+      darkTheme: darkCodeTheme,
+      theme: lightCodeTheme,
+      enableLineNumbers: true,
     },
-    footer: {
-      style: 'dark',
-      links: [
-        {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Style Guide',
-              to: 'docs/doc1',
-            },
-            {
-              label: 'Second Doc',
-              to: 'docs/doc2',
-            },
-          ],
-        },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
-            },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'Blog',
-              to: 'blog',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
-            },
-          ],
-        },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+    logo: {
+      alt: 'Libra Logo',
+      to: 'https://libra.org/'
+    },
+    sidebarCollapsible: false,
+    siteID: 'developers',
+    navbar: {
+      title: 'Libra Docs',
     },
   },
   plugins: [
