4402d13efd9399890c922b13b695c40ab4440edc
oriooctopus <oliverullman@gmail.com>
2020-06-15 15:09:13 -0600

    LIB-712: Requested changes - move pagination to own file

--- src/theme/DocItem/index.js
+++ src/theme/DocItem/index.js
@@ -1,45 +1,16 @@
 import React from 'react';
 
-import DocPaginator from '@theme/DocPaginator';
 import Head from '@docusaurus/Head';
 import isInternalUrl from '@docusaurus/isInternalUrl';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import useTOCHighlight from '@theme/hooks/useTOCHighlight';
 
+import Pagination from './Pagination';
 import {RightSidebar} from 'libra-docusaurus';
-import ArrowLeft from 'img/shared/arrow-left.svg';
-import ArrowRight from 'img/shared/arrow-right.svg';
 
 import classnames from 'classnames';
 import styles from './styles.module.css';
-
-const Pagination = ({metadata}) => {
-  const previousLink = metadata;
-
-  return (
-    <div className={styles.pagination}>
-      <a 
-        className={classnames(styles.previous, {
-          [styles['disabled']]: !metadata.previous,
-        })} 
-        href={metadata.previous && metadata.previous.permalink}
-      >
-        <ArrowLeft />
-        <span>Previous</span>
-      </a>
-      <a 
-        className={classnames(styles.next, {
-          [styles['disabled']]: !metadata.next,
-        })} 
-        href={metadata.next && metadata.next.permalink}
-      >
-        <span>Next</span>
-        <ArrowRight />
-      </a>
-    </div>
-  );
-};
 
 function DocItem(props) {
   const {siteConfig = {}} = useDocusaurusContext();
