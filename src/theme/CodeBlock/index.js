905a81740e499cd37ee885ff072d1b719b02cb97
oriooctopus <oliverullman@gmail.com>
2020-07-02 13:32:23 -0600

    LIB-752: Add border and tighten up border radiuses on snippets

--- src/theme/CodeBlock/index.js
+++ src/theme/CodeBlock/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import CodeBlock from '@theme-original/CodeBlock';
 
+import classnames from 'classnames';
+
 const Wrapper = ({isWithinTab, learnMoreLink, withinMultiStep, ...props}) => {
   if (!withinMultiStep && isWithinTab) {
     return <CodeBlock {...props} />
   }
   
   return (
-    <div className="snippet-container">
+    <div className={classnames("snippet-container", {
+      'is-within-multi-step': withinMultiStep,
+    })}>
       <div className="snippet-header">
         {learnMoreLink &&
           <a className="learn-more" href={learnMoreLink}>
