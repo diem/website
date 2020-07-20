091c51b97b4758cb1da9effc3cc84d700d734f12
oriooctopus <oliverullman@gmail.com>
2020-07-20 12:44:35 -0600

    LIB-782: Add syntax highlighting for rust

--- src/theme/CodeBlock/index.js
+++ src/theme/CodeBlock/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import {Prism} from 'prism-react-renderer';
 import CodeBlock from '@theme-original/CodeBlock';
 
 import classnames from 'classnames';
+
+(typeof global !== "undefined" ? global : window).Prism = Prism;
+require("prismjs/components/prism-rust");
 
 const Wrapper = ({isWithinTab, learnMoreLink, withinMultiStep, ...props}) => {
   if (!withinMultiStep && isWithinTab) {
     return <CodeBlock {...props} />
   }
-  
+
   return (
     <div className={classnames("snippet-container", {
       'is-within-multi-step': withinMultiStep,
